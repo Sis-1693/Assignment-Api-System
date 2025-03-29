90ec3f291d75951098aeeb383036cf628f1e3983
SalmanIbrahimShaikh <salmanshaikh1693@gmail.com>
2025-03-30 01:43:41 +0530

    Updated API and fixed issues

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
+use App\Models\Order;
+use App\Http\Controllers\API\OrderController;
 use App\Http\Controllers\API\CheckoutController;
 
 
@@ -16,4 +18,11 @@ Route::prefix('cart')->group(function () {
 });
 
 
-Route::post('/checkout', [CheckoutController::class, 'checkout']);
+Route::get('/orders', [OrderController::class, 'index']); // Get all orders
+Route::post('/orders', [OrderController::class, 'store']); // Create order
+Route::get('/orders/{id}', [OrderController::class, 'show']); // Get order details
+Route::put('/orders/{id}', [OrderController::class, 'update']); // Update order
+Route::delete('/orders/{id}', [OrderController::class, 'destroy']); // Delete order
+
+Route::post('/checkout', [CheckoutController::class, 'checkout']); // Checkout with Stripe
+
